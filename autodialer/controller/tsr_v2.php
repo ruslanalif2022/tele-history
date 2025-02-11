3a4a4445607e5cd2fa6028fd1085893b9aaf6bfd
ruslanalif2022 <ruslanalif2022@gmail.com>
2025-02-11 11:25:59 +0700

    first kommit from new user ruslanalif2022

--- autodialer/controller/tsr_v2.php
+++ autodialer/controller/tsr_v2.php
@@ -45,6 +45,7 @@ class Tsr_v2 extends Controller
     {
         //$this->output->enable_profiler(TRUE);
         // die('haha');
+        // test publish
         $data["txt_title"] = "Tele Dashboard";
         $post = @$_POST["post"];
 
